673a745dec43683a950033914e9833cc7caf3059
Samarth Bhadane <samarthbhadane119@gmail.com>
2024-11-20 02:47:25 -0800

    Add Speed Insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,7 @@ import { ThemeProvider } from "@/context/ThemeContext";
 import { usePathname } from "next/navigation";
 import Footer from "@/components/Footer";
 import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 
 
@@ -23,6 +24,7 @@ export default function RootLayout({
           <Navbar DisplayName={Home ? false : true}/>
           <div className="flex justify-center items-center mt-16 p-8 max-w-full md:max-w-[70%] min-h-screen mx-auto">{children}</div>
           <Footer/>
+          <SpeedInsights />
           <Analytics/>
         </ThemeProvider>
       </body>
