a662d70a9bc774686499af2c766caf4bb8ff43d5
Samarth Bhadane <samarthbhadane119@gmail.com>
2024-11-20 02:42:22 -0800

    Add Vercel Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import "./globals.css";
 import { ThemeProvider } from "@/context/ThemeContext";
 import { usePathname } from "next/navigation";
 import Footer from "@/components/Footer";
+import { Analytics } from "@vercel/analytics/next";
 
 
 
@@ -22,6 +23,7 @@ export default function RootLayout({
           <Navbar DisplayName={Home ? false : true}/>
           <div className="flex justify-center items-center mt-16 p-8 max-w-full md:max-w-[70%] min-h-screen mx-auto">{children}</div>
           <Footer/>
+          <Analytics/>
         </ThemeProvider>
       </body>
     </html>
